76b026d395cbb90f58caf456919f577c219520d4
Josh-Goble <130126786+Josh-Goble@users.noreply.github.com>
2023-05-06 23:55:09 -0700

    Cleaned up code

--- M05/example.js
+++ M05/example.js
@@ -1,24 +1,12 @@
-
-var table = prompt('Enter number'); // Unit of table
-var operator = 'multiplication'; // Type of calculation
+var table = prompt('Enter number'); // ask user for unit of table
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>'; // Message
 
-  
-
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
