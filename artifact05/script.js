4a052dfbbdd66db55e81201015cb389a88c3635b
Josh-Goble <130126786+Josh-Goble@users.noreply.github.com>
2023-06-06 00:33:32 -0700

    Added previous function

--- artifact05/script.js
+++ artifact05/script.js
@@ -1,3 +1,4 @@
+var caption = document.getElementById("caption");
 var currentPic = kitten;
 
 function previous() {
@@ -5,34 +6,82 @@ function previous() {
     document.getElementById("kitten").className = "hide";
     document.getElementById("lion").className = "slide";
     currentPic = lion;
+    caption.innerHTML = "Lion";
+    
+  }
+  else if (currentPic == lion) {
+    document.getElementById("lion").className = "hide";
+    document.getElementById("tiger").className = "slide";
+    currentPic = tiger;
+    caption.innerHTML = "Tiger";
+    
+  }
+  else if (currentPic == tiger) {
+    document.getElementById("tiger").className = "hide";
+    document.getElementById("lynx").className = "slide";
+    currentPic = lynx;
+    caption.innerHTML = "Lynx";
+    
+  }
+  else if (currentPic == lynx) {
+    document.getElementById("lynx").className = "hide";
+    document.getElementById("cat").className = "slide";
+    currentPic = cat;
+    caption.innerHTML = "Cat";
+    
+  }
+  else if (currentPic == cat) {
+    document.getElementById("cat").className = "hide";
+    document.getElementById("kitten").className = "slide";
+    currentPic = kitten;
+    caption.innerHTML = "Kitten";
     
   }
 }
 
 function next() {
+
   if (currentPic == kitten) {
     document.getElementById("kitten").className = "hide";
     document.getElementById("cat").className = "slide";
     currentPic = cat;
+    caption.innerHTML = "Cat";
     
   }
-  if (currentPic == cat) {
+   else if (currentPic == cat) {
     document.getElementById("cat").className = "hide";
     document.getElementById("lynx").className = "slide";
     currentPic = lynx;
+    caption.innerHTML = "Lynx";
     
   }
-  if (currentPic == lynx) {
+   else if (currentPic == lynx) {
     document.getElementById("lynx").className = "hide";
     document.getElementById("tiger").className = "slide";
     currentPic = tiger;
+    caption.innerHTML = "Tiger";
     
   }
-  if (currentPic == tiger) {
+  else if (currentPic == tiger) {
     document.getElementById("tiger").className = "hide";
     document.getElementById("lion").className = "slide";
     currentPic = lion;
+    caption.innerHTML = "lion";
     
   }
+   else if (currentPic == lion) {
+    document.getElementById("lion").className = "hide";
+    document.getElementById("kitten").className = "slide";
+    currentPic = kitten;
+    caption.innerHTML = "Kitten";
+    
+  }
+  
+ 
+  
+    
+   
+  
+
   
 }
